5868782af75d39c11e780baf82ba2a646b8f0721
Asim Aslam <asim@chuhnk.me>
2018-08-21 18:05:35 +0100

    blob interface

--- blob/blob.go
+++ blob/blob.go
@@ -1,1 +1,30 @@
+// Package blob represents blob storage
 package blob
+
+import (
+	"io"
+)
+
+// Store represents blob storage
+type Store interface {
+	// Operations on Buckets
+	Create(string) error
+	Delete(string) error
+	Get(string) (Bucket, error)
+	List() ([]Bucket, error)
+}
+
+// type Bucket represents a bucket in the store
+type Bucket interface {
+	// Operations on Objects
+	Create(string) (Blob, error)
+	Delete(string) error
+	Get(string) (Blob, error)
+}
+
+type Blob interface {
+	Name() string
+	Metadata() map[string]interface{}
+	Read() io.ReadCloser
+	Write() io.Writer
+}
