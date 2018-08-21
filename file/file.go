71ac7d3c90e815970efd3ca57163a9b69708e502
Asim Aslam <asim@chuhnk.me>
2018-08-21 17:35:07 +0100

    file interface

--- file/file.go
+++ file/file.go
@@ -1,1 +1,10 @@
+// type File represents file access to a resource
 package file
+
+// type File represents a file
+type File interface {
+	Read(b []byte) (int, error)
+	Write(b []byte) (int, error)
+	Seek(off int64, when int) (int64, error)
+	Close() error
+}
