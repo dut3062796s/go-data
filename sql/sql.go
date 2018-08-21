f361677716f03c1301565c42f7029d86364c525a
Asim Aslam <asim@chuhnk.me>
2018-08-21 18:10:24 +0100

    add sql interface

--- sql/sql.go
+++ sql/sql.go
@@ -1,1 +1,17 @@
+// Package sql provides a SQL interface
 package sql
+
+type DB interface {
+	Exec(query string, args ...interface{}) error
+	Query(query string, args ...interface{}) (Rows, error)
+	Close() error
+}
+
+type Rows interface {
+	Close() error
+	Next() (Row, error)
+}
+
+type Row interface {
+	Scan(v interface{}) error
+}
