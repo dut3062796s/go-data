a16aee060c7dada232090c84b4e78bfb439737f8
Asim Aslam <asim@chuhnk.me>
2018-08-21 15:04:29 +0100

    add kv interface

--- kv/kv.go
+++ kv/kv.go
@@ -1,1 +1,27 @@
+// Package kv provides a keyvalue interface
 package kv
+
+// KV provides a simple keyvalue interface
+type KV interface {
+	Close() error
+	Get(key []byte) (Value, error)
+	Put(key []byte, val interface{}) error
+	Del(key []byte) error
+	Scan(start, end []byte) Scanner
+}
+
+// Scanner is used for range scanning
+type Scanner interface {
+	Close() error
+	Next() bool
+	Key() []byte
+	Val() Value
+	Err() error
+}
+
+// Value is returned from the DB
+type Value interface {
+	Key() []byte
+	Bytes() []byte
+	Scan(v interface{}) error
+}
